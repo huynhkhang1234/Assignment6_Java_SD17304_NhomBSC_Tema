c807f0c843f19c93034e30ceb5cbe0353302af96
PiKayQi Nguyễn <101205004+PKQNguyen@users.noreply.github.com>
2023-07-20 20:20:02 +0700

    commit login, index

--- src/main/java/com/poly/Controller/admin/NewsMANController.java
+++ src/main/java/com/poly/Controller/admin/NewsMANController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.poly.DAO.CategoriesDAO;
